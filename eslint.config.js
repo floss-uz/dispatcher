90c38d97b81871c3b74bafb844aef903b1c11692
3nln <javohirtech@gmail.com>
2025-10-23 19:24:29 +0500

    chore: prettier configured

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import prettier from 'eslint-plugin-prettier'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import prettier from "eslint-plugin-prettier"
+import { defineConfig, globalIgnores } from "eslint/config"
 
 export default defineConfig([
-  globalIgnores(['dist', 'build']),
+  globalIgnores(["dist", "build"]),
   {
-    files: ['**/*.{ts,tsx,js,jsx}'],
+    files: ["**/*.{ts,tsx,js,jsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     plugins: {
@@ -21,7 +21,7 @@ export default defineConfig([
     },
     rules: {
       ...prettier.configs.recommended.rules,
-      'prettier/prettier': 'warn',
+      "prettier/prettier": "warn",
     },
     languageOptions: {
       ecmaVersion: 2020,
