ebefd1271620aa2e973dc5e3efe620308c26e444
3nln <javohirtech@gmail.com>
2025-10-23 11:20:46 +0500

    chore: i18n translation init

--- src/app/DashboardPage.tsx
+++ src/app/DashboardPage.tsx
@@ -1,13 +1,25 @@
 import { Button, useColorScheme } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 export const DashboardPage = () => {
+  // theme
   const { mode, setMode } = useColorScheme();
+
+  // translate
+  const { t, i18n } = useTranslation();
+  const toggleLang = () => {
+    i18n.changeLanguage(i18n.language === "en" ? "uz" : "en");
+  };
+
   return (
     <div>
       uy! {JSON.stringify(mode)}
       <Button onClick={() => setMode(mode === "dark" ? "light" : "dark")}>
         Hello world
       </Button>
+      <h1>{t("name")}</h1>
+      <p>til: {i18n.language}</p>
+      <Button onClick={toggleLang}>change</Button>
     </div>
   );
 };
--- src/shared/config/i18n.ts
+++ src/shared/config/i18n.ts
@@ -2,3 +2,26 @@
  *
  * @description global i18n config
  */
+
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpBackend from "i18next-http-backend";
+
+i18n
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    supportedLngs: ["en", "uz"],
+    debug: import.meta.env.DEV,
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}.json",
+    },
+  });
+
+export { i18n };
