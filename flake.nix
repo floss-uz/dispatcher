459837c7c5c6c2eaf235b2a50498e5d14f4ca978
3nln <javohirtech@gmail.com>
2025-10-23 14:11:32 +0500

    fix: nix formatter config

--- flake.nix
+++ flake.nix
@@ -4,21 +4,56 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    #    Git hooks
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs = {
+    self,
     nixpkgs,
     flake-utils,
+    pre-commit-hooks,
+    treefmt-nix,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
-    in {
-      devShells.default = pkgs.mkShell {
-         packages = with pkgs;[
-            nodejs_24
-            zip
-          ];
+      treefmt = treefmt-nix.lib.mkWrapper pkgs {
+        projectRootFile = "flake.nix";
+        programs = {
+          prettier.enable = true;
+          alejandra.enable = true;
         };
+      };
+
+      preCommit = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          prettier.enable = true;
+          eslint.enable = true;
+          alejandra.enable = true;
+        };
+      };
+    in {
+      checks.pre-commit-check = preCommit;
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_24
+          zip
+          treefmt
+          git
+        ];
+        shellHook = ''
+          export PATH=$PWD/node_modules/.bin:$PATH
+          echo "react dev-shell loaded node:$(node -v)"
+        '';
+      };
     });
 }
