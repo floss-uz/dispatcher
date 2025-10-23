3080a2a1462cbad89ca0035795485856aed5652d
3nln <javohirtech@gmail.com>
2025-10-23 19:19:09 +0500

    chore: prettier configured

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,31 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist', 'build']),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      ...prettier.configs.recommended.rules,
+      'prettier/prettier': 'warn',
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-]);
+])
--- src/shared/config/i18n.ts
+++ src/shared/config/i18n.ts
@@ -3,25 +3,25 @@
  * @description global i18n config
  */
 
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import HttpBackend from "i18next-http-backend";
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import HttpBackend from 'i18next-http-backend'
 
 i18n
   .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "en",
-    supportedLngs: ["en", "uz"],
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'uz'],
     debug: import.meta.env.DEV,
     interpolation: {
       escapeValue: false,
     },
     backend: {
-      loadPath: "/locales/{{lng}}.json",
+      loadPath: '/locales/{{lng}}.json',
     },
-  });
+  })
 
-export { i18n };
+export { i18n }
