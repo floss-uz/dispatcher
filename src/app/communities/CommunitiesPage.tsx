1e6029e850622fb5264e1878deee75d846f9a276
Javoxir G'ulomjonov <javohirtech@gmail.com>
2025-11-05 22:49:21 +0500

    feat: reuseable table component (#8)
    * chore: init project and router setup
    
    * chore: husky configured
    
    * chore: husky configured
    
    * chore: husky configured
    
    * chore: nix init
    
    * chore: mui init
    
    * chore: i18n translation init
    
    * fix: gitignore
    
    * fix: nix formatter config
    
    * chore: prettier configured
    
    * chore: prettier configured
    
    * chore: prettier configured
    
    * feat: sidebar and router setup
    
    * fix: unused lint error
    
    * feat: init table
    
    * fix: layout problem and mui changes
    
    * feat: new hook useAutoScroll
    
    * fix: remove package-lock.json

--- src/app/communities/CommunitiesPage.tsx
+++ src/app/communities/CommunitiesPage.tsx
@@ -1,3 +1,50 @@
+import { MyTable } from "@/shared/ui"
+import { GridColDef, GridRowsProp } from "@mui/x-data-grid"
+
+const rows: GridRowsProp = [
+  { id: 1, name: "Data Grid", description: "the Community version" },
+  { id: 2, name: "Data Grid Pro", description: "the Pro version" },
+  { id: 3, name: "Data Grid Premium", description: "the Premium version" },
+  { id: 1, name: "Data Grid", description: "the Community version" },
+  { id: 2, name: "Data Grid Pro", description: "the Pro version" },
+  { id: 3, name: "Data Grid Premium", description: "the Premium version" },
+  { id: 1, name: "Data Grid", description: "the Community version" },
+  { id: 2, name: "Data Grid Pro", description: "the Pro version" },
+  { id: 3, name: "Data Grid Premium", description: "the Premium version" },
+  { id: 1, name: "Data Grid", description: "the Community version" },
+  { id: 2, name: "Data Grid Pro", description: "the Pro version" },
+  { id: 3, name: "Data Grid Premium", description: "the Premium version" },
+  { id: 1, name: "Data Grid", description: "the Community version" },
+  { id: 2, name: "Data Grid Pro", description: "the Pro version" },
+  { id: 3, name: "Data Grid Premium", description: "the Premium version" },
+]
+
+const columns: GridColDef[] = [
+  {
+    field: "name",
+    headerName: "Product Name",
+    width: 200,
+  },
+  { field: "description", headerName: "Description", width: 300 },
+  { field: "description", headerName: "Description", width: 300 },
+  { field: "description", headerName: "Description", width: 300 },
+  { field: "description", headerName: "Description", width: 300 },
+  { field: "description", headerName: "Description", width: 300 },
+  { field: "description", headerName: "Description", width: 300 },
+  { field: "description", headerName: "Description", width: 300 },
+]
+
 export const CommunitiesPage = () => {
-  return <div>CommunitiesPage</div>
+  return (
+    <div>
+      <MyTable
+        title={"Jamiyatlar"}
+        checkboxSelection
+        disableRowSelectionOnClick
+        showToolbar
+        rows={rows}
+        columns={columns}
+      />
+    </div>
+  )
 }
